441743fe6ebd63033b16fd9033c6fbb1605ffada
myouo <chizurumyo@gmail.com>
2026-02-08 02:27:18 +0800

    删掉重构

--- crates/rocoknight-core/src/update.rs
+++
@@ -1,6 +0,0 @@
-use crate::error::{CoreError, CoreResult};
-
-pub async fn check_for_updates(_endpoint: &str) -> CoreResult<()> {
-    Err(CoreError::Network("update not implemented".to_string()))
-}
-
